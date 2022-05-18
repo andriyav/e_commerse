9978a3ec1afe49df439558a5fb8ec359cf796253
andriyav@hotmail.com <andriyav@hotmail.com>
2022-05-19 00:28:50 +0300

    models, views

--- djecommerce/core/views.py
+++ djecommerce/core/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Item
 
 
 
-def item_list(request):
-    context = {
-        'items': Item.objects.all()
-    }
-    return render(request, 'core/item_list.html', context)
+class HomeView(ListView):
+    model = Item
+    template_name = "core/home.html"
 
-# Create your views here.
+
+class ItemViewDetail(DetailView):
+    model = Item
+    template_name = "core/product.html"
