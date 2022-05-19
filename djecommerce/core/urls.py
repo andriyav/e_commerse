33f9defbf8d3bc75f2019cc6f952f29ce8da9d20
andriyav@hotmail.com <andriyav@hotmail.com>
2022-05-19 22:49:52 +0300

    add to cart

--- djecommerce/core/urls.py
+++ djecommerce/core/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import (
     HomeView,
-    ItemViewDetail
+    ItemViewDetail, add_to_cart
 )
 
 app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('product/<slug>/', ItemViewDetail.as_view(), name='product')
+    path('product/<slug>/', ItemViewDetail.as_view(), name='product'),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart')
 ]
