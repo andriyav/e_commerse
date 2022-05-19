7c6f59fbb50bd00bc5ec8ec33801c182abb78043
andriyav@hotmail.com <andriyav@hotmail.com>
2022-05-20 01:02:26 +0300

    remove from cart

--- djecommerce/core/urls.py
+++ djecommerce/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     HomeView,
-    ItemViewDetail, add_to_cart
+    ItemViewDetail, add_to_cart, remove_from_cart
 )
 
 app_name = 'core'
@@ -9,5 +9,6 @@ app_name = 'core'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('product/<slug>/', ItemViewDetail.as_view(), name='product'),
-    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart')
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
 ]
