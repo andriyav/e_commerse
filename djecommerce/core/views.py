4c76a135158dc69cdbca0d71418dcd8aeb6bcbd7
andriyav@hotmail.com <andriyav@hotmail.com>
2022-07-07 00:14:24 +0300

    messages.info

--- djecommerce/core/views.py
+++ djecommerce/core/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.views.generic import ListView, DetailView
@@ -27,14 +28,18 @@ def add_to_cart(request, slug):
         if order.items.filter(item__slug=item.slug).exists():
             order_item.quantity += 1
             order_item.save()
+            messages.info(request, "Кількість товару змінена")
+            return redirect("core:product", slug=slug)
         else:
+            messages.info(request, "Товар був доданий до корзини")
             order.items.add(order_item)
+            return redirect("core:product", slug=slug)
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(
             user=request.user, ordered_date=ordered_date)
         order.items.add(order_item)
-    return redirect("core:product", slug=slug)
+        return redirect("core:product", slug=slug)
 
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
@@ -51,9 +56,11 @@ def remove_from_cart(request, slug):
                 ordered=False
             )[0]
             order.items.remove(order_item)
-            print('removed')
+            messages.info(request, "Товар видалено з корзини")
+            return redirect("core:product", slug=slug)
         else:
+            messages.info(request, "Товар не в корзині")
             return redirect("core:product", slug=slug)
     else:
+        messages.info(request, "Всі замовлення скасовані")
         return redirect("core:product", slug=slug)
-    return redirect("core:product", slug=slug)
