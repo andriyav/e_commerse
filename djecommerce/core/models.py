7dc2f54401faedc76199e0b436ff25c2c4e3c74d
andriyav@hotmail.com <andriyav@hotmail.com>
2022-07-09 17:40:34 +0300

    messeges UA

--- djecommerce/core/models.py
+++ djecommerce/core/models.py
@@ -18,11 +18,9 @@ LABEL_CHOICES = (
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
-    discount_price = models.FloatField(blank=True, null=True)
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=3)
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     slug = models.SlugField()
-    description = models.TextField()
 
     def __str__(self):
         return self.title
@@ -31,27 +29,13 @@ class Item(models.Model):
         return reverse("core:product", kwargs={
             'slug': self.slug
         })
-    def get_add_to_cart_url(self):
-        return reverse("core:add-to-cart", kwargs={
-            'slug': self.slug
-        })
-
-    def get_remove_from_cart_url(self):
-        return reverse("core:remove-from-cart", kwargs={
-            'slug': self.slug
-        })
 
 
 class OrderItem(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE
-                             )
-    ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return f'{self.quantity} of {self.item.title}'
+        return self.title
 
 
 class Order(models.Model):
--- djecommerce/core/urls.py
+++ djecommerce/core/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 from .views import (
     HomeView,
-    ItemViewDetail, add_to_cart, remove_from_cart
+    ItemViewDetail
 )
 
 app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('product/<slug>/', ItemViewDetail.as_view(), name='product'),
-    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
-    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('product/<slug>/', ItemViewDetail.as_view(), name='product')
 ]
